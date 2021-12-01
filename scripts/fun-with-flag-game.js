c6e4d07b139e944d702e5c3db12d2615eca5dea7
Louis Flores <louisdefleurs@gmail.com>
2021-11-30 23:43:45 -0500

    Random code generator working

--- scripts/fun-with-flag-game.js
+++ scripts/fun-with-flag-game.js
@@ -1,9 +1,63 @@
-// axios.get('https://flagcdn.com/en/codes.json')
+
+let countryCodes = [];
+let countryNames = [];
+
+const showCountryFlag = (countries) => {
+
+  // returns all keys in the object as an array
+  countryCodes = Object.keys(countries);
+
+  //returns all values in object as an array
+  countryNames = Object.values(countries);
+
+  // random integer generator
+  const getRandomInt = (maxnumber) => {
+    maxnumber = countryCodes.length;
+    return Math.floor(Math.random() * (maxnumber))
+  };
+
+  randomCode = countryCodes[getRandomInt()];
+
+  // for (let countryCode in countries) {
+  //   // returns country code (i.e. "az")
+  //   console.log(countryCode);
+
+  //   // returns country name
+  //   console.log(countries[countryCode])
+  // }
+
+
+};
+
+
+
+
+
+axios.get('https://flagcdn.com/en/codes.json')
+  .then((response) => {
+    return response.data;
+  })
+  .then(data => {
+    //about 306 different countries
+    showCountryFlag(data)
+  })
+  ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// axios.get('https://flagcdn.com/za.svg')
 //   .then((response) => {
 //     console.log(response)
 //   })
-
-axios.get('https://flagcdn.com/za.svg')
-  .then((response) => {
-    console.log(response)
-  })
