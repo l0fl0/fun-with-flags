2d013cfd87b69361625f40945d3787d742a8ce00
Louis Flores <louisdefleurs@gmail.com>
2022-07-14 19:21:05 -0400

    cleaned up variables *Noted redundancy with memory variables and localstorage data

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,55 +1,21 @@
 let apiResponse = {};
-
 let countryCodes = [];
-
 let countryNames = [];
-
 let countryCodesWithoutStates = [];
-
 let correctCountry = "";
-
 let indexOfStates = [];
-
-// return an array of counrty codes
 let incorrectFlags = [];
-
-// return an array of counrty codes
 let correctFlags = [];
-
 let countryCode = "";
-
-let timeLimit = 3000;
-
+let timeLimit = 10000;
+let timerCountdown = 0;
+let users = [];
 let user = {
   name: "",
   score: 0,
   lives: 5
 };
 
-/*
-local storage of previos single games
-*/
-let users = [];
 // check to see if storage exists and then set user obj to local storage
 if (JSON.parse(localStorage.getItem("users"))) users = JSON.parse(localStorage.getItem("users"));
 
-/*
- Defines the variables from API and saves to local storage
-*/
-const define = (data) => {
-
-  localStorage.setItem("countryCodes", JSON.stringify(Object.keys(data)))
-  localStorage.setItem("countryNames", JSON.stringify(Object.values(data)));
-
-  countryCodes = JSON.parse(localStorage["countryCodes"]);
-
-  countryCodesWithoutStates = countryCodes.slice().filter(
-    (code, i) => {
-      !(code.startsWith("us-"))
-      indexOfStates.push(i)
-    });
-
-  localStorage.setItem("apiResponse", JSON.stringify(data));
-
-  apiResponse = JSON.parse(localStorage["apiResponse"]);
-};
