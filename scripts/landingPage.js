328bb878e3d190a32a43ab5b4543c1d6e158d45b
Louis Flores <louisdefleurs@gmail.com>
2022-07-14 19:23:00 -0400

    added start sequence for the form submit

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -1,26 +1,9 @@
-import { createPageElement } from "../utils/utils.js";
-
+import { startGame } from "./fun-with-flag-game.js";
 
 const howToEl = document.querySelector(".how-to-play");
 const registerEl = document.querySelector(".register");
 const gameEl = document.querySelector(".fwf-game");
 
-const userInfoContainer = document.querySelector(".fwf-game__user")
-function buildUserInfo(user) {
-
-  // let user = JSON.parse(localStorage["user"]);
-  // User info 
-  const userName = createPageElement("div", "fwf-game__user-name", `Player: ${user.name}`);
-  userInfoContainer.appendChild(userName);
-
-  //   // lives 
-  //   const userLifeElement = createPageElement("div", "fwf-game__user-lives", `Lives: ${user.lives}`);
-  //   userInfoContainer.appendChild(userLifeElement);
-
-  //   // score 
-  //   const userScore = createPageElement("div", "fwf-game__user-score", `Score: ${user.score}`);
-  //   userInfoContainer.appendChild(userScore);
-};
 
 function hideSections() {
   howToEl.classList.add("hide");
@@ -35,17 +18,11 @@ function showGame() {
 function handleSubmit(event) {
   event.preventDefault();
   user.name = event.target.name.value;
-  // localStorage.setItem("user", JSON.stringify(user));
-
-
   setTimeout(() => {
     hideSections();
-    showGame()
+    showGame();
+    startGame();
   }, 500)
-
-  buildUserInfo(user)
 };
 
 const formEl = document.querySelector(".register__form");
-
-formEl.addEventListener("submit", handleSubmit);
