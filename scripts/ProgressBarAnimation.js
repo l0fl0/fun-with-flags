ebcaf486a1c939aaeaf1e5f6fc1bc51ec19e14f5
Louis Flores <louisdefleurs@gmail.com>
2022-07-20 17:05:13 -0400

    added a progress bar for countdown timer

---
+++ scripts/ProgressBarAnimation.js
@@ -0,0 +1,19 @@
+export function move(progressTime) {
+  let width = 100;
+  let elem = document.getElementById("barStatus")
+  elem.classList.remove("user-info__barstatus--middle", "user-info__barstatus--end")
+
+  let id = setInterval(frame, progressTime / 100);
+
+  function frame() {
+    if (width === 1) clearInterval(id)
+    if (width === 60) elem.classList.add("user-info__barstatus--middle");
+    if (width === 30) elem.classList.add("user-info__barstatus--end");
+
+    width--;
+    elem.style.width = width + '%';
+  }
+
+
+}
+
