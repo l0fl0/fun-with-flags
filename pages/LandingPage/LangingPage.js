57fe779362606996ed32c4e653b4625c35f7b932
Louis Flores <louisdefleurs@gmail.com>
2022-07-28 19:25:01 -0400

    style fixes for header and id name change

--- pages/LandingPage/LangingPage.js
+++ pages/LandingPage/LangingPage.js
@@ -13,12 +13,11 @@ const formEl = document.querySelector(".registration-form");
 formEl.addEventListener("submit", handleSubmit);
 
 const difficultyOptions = document.querySelectorAll(".registration-form__game-option");
-difficultyOptions.forEach(el => el.addEventListener("click", activeDifficulty))
+difficultyOptions.forEach(el => el.addEventListener("click", activeDifficulty));
 
 function activeDifficulty(event) {
   difficultyOptions.forEach(el => el.labels[0].classList.remove("registration-form__label--active"));
-
-  event.target.labels[0].classList.add("registration-form__label--active")
+  event.target.labels[0].classList.add("registration-form__label--active");
 }
 
 // Form submit handler
@@ -26,12 +25,14 @@ function handleSubmit(event) {
   event.preventDefault();
 
   // Store registration information
-  user.name = event.target.name.value;
+  user.name = event.target.username.value;
   user.difficulty = event.target.difficulty.value;
 
   localStorage.setItem("user", JSON.stringify(user));
 
   //open game
   window.location.assign("./pages/FWFGame/index.html");
-
 };
+
+let usernameInput = document.getElementById("username");
+usernameInput.focus();
