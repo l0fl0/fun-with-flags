e8e3be3de612181d7b3048682c252cd28bcd4397
Louis Flores <louis.flores@loflo.dev>
2023-05-30 20:08:28 -0400

    yes

--- scripts/inersectionObserver.js
+++ scripts/inersectionObserver.js
@@ -1,11 +1,11 @@
 const options = {
 	root: null,
-	rootMargin: "-10px",
+	rootMargin: "-0.625rem",
 	threshold: 0,
 };
 
 const questionObserver = new IntersectionObserver(function (entries, observer) {
-	entries.forEach((entry) => console.log(entry.target.));
+	entries.forEach((entry) => console.log(entry.target));
 }, options);
 
 export const attachObservers = (questions) => {
